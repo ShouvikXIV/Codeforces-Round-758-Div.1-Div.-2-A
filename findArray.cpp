eeb6f3f20c2f27775f0125de19ad522c5db57d50
shouvik-barua <85792552+shouvik-barua@users.noreply.github.com>
2021-12-11 18:51:16 +0600

    Add files via upload

---
+++ findArray.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int t;
+	cin>>t;
+	for(int i = 0;i<t;i++)
+	{
+		int n;
+		cin>>n;
+		if(n==1)
+		{
+			cout<<1<<'\n';
+		}
+		else
+		{
+			int f = 2;
+			for(int k = 0;k<n;k++)
+			{
+				cout<<f<<' ';
+				f+=1;
+
+			}
+			cout<<'\n';
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}   
